cf7e4d54492140759b2d621cff031a67b933685d
hanlei24 <hanlei6@jd.com>
2016-12-16 15:54:16 +0800

    litchi

---
+++ src/main/java/com/fruit/LitchiApplication.java
@@ -0,0 +1,23 @@
+package com.fruit;
+
+import com.fruit.filter.AccessFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZuulProxy
+@SpringCloudApplication
+@EnableZipkinServer
+public class LitchiApplication {
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LitchiApplication.class, args);
+    }
+}
